ff54ad8b6c32f700e5acb08ed190e3e5c9e2af93
Anastasiya <nasta.shadrina99@gmail.com>
2024-07-18 01:07:41 +0700

    2 module, 1 task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,25 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+function ascSorting(a, b) {
+    const result = a.localeCompare(b, {sensitivity: "base"});
+    return a.toLowerCase() === b.toLowerCase() && result > 0 ? -1 : result;
+}
+
+export function sortStrings(arr, param = 'asc') {
+    let result;
+    const newArray = [...arr];
+
+    const sorting = newArray.sort((a, b)=> ascSorting(a, b));
+    
+    if (param === 'asc') {
+        result = sorting;
+    }
+
+    if (param === 'desc') {
+        result = sorting.reverse();
+    }
+
+    return result;
 }
