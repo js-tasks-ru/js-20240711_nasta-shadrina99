06671ec7a7d7f6d5b468f13221b9c99de4c6f7eb
Anastasiya <nasta.shadrina99@gmail.com>
2024-07-20 21:08:10 +0700

    2 module, 1 task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const propertiesArray = path.split('.');
 
+    return (obj) => {
+        let currentPath = obj;
+
+        for (let i = 0; i < propertiesArray.length; i++) {
+            if (typeof currentPath === 'undefined') {
+                return;
+            }
+
+            if (obj.hasOwnProperty(propertiesArray[i])) {
+                returncurrentPath = currentPath[propertiesArray[i]];
+            } else return;
+        }
+
+        return currentPath;
+    };
 }
