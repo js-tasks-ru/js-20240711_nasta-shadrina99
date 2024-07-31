f8b54dce83b93705e884343dd1efaf78ec9d7ff1
Anastasiya <nasta.shadrina99@gmail.com>
2024-07-31 17:19:37 +0700

    5 module, 1 task (part 1)

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
 export default class NotificationMessage {
+    static lastShownComponent;
 
+    constructor(message = '', props = {}) {
+        this.message = message;
+        this.props = props;
+        this.elem = this.show();
+    }
+
+    getTemplate() {
+        const {type = '', duration = 0} = this.props;
+
+        return `
+            <div class="notification ${type}" style="--value:${duration / 1000}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${type}</div>
+                    <div class="notification-body">${this.message}</div>
+                </div>
+            </div>
+        `;
+    }
+
+    createElem() {
+        const div = document.createElement('div');
+        div.innerHTML = this.getTemplate();
+        const firstElementChild = div.firstElementChild;
+        return firstElementChild;
+    }
+
+    show() {
+        this.hideUnnecessaryElements();
+        this.elem = this.createElem();
+        document.body.append(this.elem);
+        return this.elem;
+    }
+
+    hideUnnecessaryElements() {
+        const {duration = 0} = this.props;
+
+        if (NotificationMessage.lastShownComponent) {
+            NotificationMessage.lastShownComponent.delete();
+        }
+        
+        NotificationMessage.lastShownComponent = this;
+
+        setTimeout(() => this.delete(), duration);
+    }
+
+    delete() {
+        this.elem.remove();
+    }
+
+    destroy() {
+        this.delete();
+    }
 }
